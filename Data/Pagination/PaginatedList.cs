4d638b9f6acc52a52083779b6464d09af6176cba
otipz19 <tasitwork@gmail.com>
2023-07-08 15:28:06 +0300

    Added posts repository and PaginatedList

---
+++ Data/Pagination/PaginatedList.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data.Pagination
+{
+    public class PaginatedList<T> : List<T>
+    {
+        public PaginatedList(IEnumerable<T> items, int totalPages, int currentIndex)
+        {
+            this.AddRange(items);
+            TotalPages = totalPages;
+            CurrentIndex = currentIndex;
+        }
+
+        public int TotalPages { get; private set; }
+
+        public int CurrentIndex { get; private set; }
+
+        public bool HasNext => CurrentIndex < TotalPages;
+
+        public bool HasPrevious => CurrentIndex > 0;
+
+        public static async Task<PaginatedList<T>> CreateAsync(IQueryable<T> query, int pageSize, int pageIndex)
+        {
+            List<T> items = await query.Skip(pageSize * pageIndex).Take(pageSize).ToListAsync();
+            int totalPages = (int)Math.Ceiling(await query.CountAsync() / (double)pageSize);
+            return new PaginatedList<T>(items, totalPages, pageIndex);
+        }
+    }
+}
---
+++ Data/Repositories/PostRepository.cs
@@ -0,0 +1,101 @@
+﻿using Data.Entities;
+using Data.Pagination;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Data.Repositories
+{
+    public class PostRepository
+    {
+        private DbSet<Post> _set;
+        private IQueryable<Post> _query;
+
+        public PostRepository(AppDbContext context)
+        {
+            _set = context.Posts;
+            _query = _set.AsQueryable();
+        }
+
+        public void Add(Post post)
+        {
+            _set.Add(post);
+        }
+
+        public void AddRange(IEnumerable<Post> posts)
+        {
+            _set.AddRange(posts);
+        }
+
+        public void Update(Post post)
+        {
+            _set.Update(post);
+        }
+
+        public void Remove(Post post)
+        {
+            _set.Remove(post);
+        }
+
+        public PostRepository Filter(Expression<Func<Post, bool>> filter)
+        {
+            _query.Where(filter);
+            return this;
+        }
+
+        public PostRepository IncludeAll()
+        {
+            return this.IncludeAuthor().IncludeComments().IncludeWatches().IncludeLikes().IncludeDislikes();
+        }
+
+        public PostRepository IncludeAuthor()
+        {
+            _query.Include(e => e.CreatedByUser);
+            return this;
+        }
+
+        public PostRepository IncludeComments()
+        {
+            _query.Include(e => e.Comments);
+            return this;
+        }
+
+        public PostRepository IncludeWatches()
+        {
+            _query.Include(e => e.WatchedBy);
+            return this;
+        }
+
+        public PostRepository IncludeLikes()
+        {
+            _query.Include(e => e.LikedBy);
+            return this;
+        }
+
+        public PostRepository IncludeDislikes()
+        {
+            _query.Include(e => e.DislikedBy);
+            return this;
+        }
+
+        public async Task<Post> FirstOrDefaultAsync(int id)
+        {
+            Post result = await _query.FirstOrDefaultAsync(e => e.Id == id);
+            _query = _set.AsQueryable();
+            return result;
+        }
+
+        public async Task<List<Post>> ToListAsync()
+        {
+            List<Post> result = await _query.ToListAsync();
+            _query = _set.AsQueryable();
+            return result;
+        }
+
+        public async Task<PaginatedList<Post>> ToPaginatedListAsync(int pageSize, int pageIndex)
+        {
+            PaginatedList<Post> result = await PaginatedList<Post>.CreateAsync(_query, pageSize, pageIndex);
+            _query = _set.AsQueryable();
+            return result;
+        }
+    }
+}
